9e900adfb31bdff0712535eefc23d00ebbc2e1e6
gursangam-fullstack <gursangamsingh.gjexperts@gmail.com>
2025-09-27 16:02:29 +0530

    add cors

--- app.js
+++ app.js
@@ -9,10 +9,23 @@ const port = 3001;
 
 // main app 
 app.use(bodyParser.json());
-app.use(cors())
+
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
 app.use('/', AuthRouter)
 
 app.listen(port, () => {
     console.log(`server listern on port: ${port}`);
-    
+
 })
